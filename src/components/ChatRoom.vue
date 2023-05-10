563f03c2ef5161629c8d41bbc40a95d99f28dfe7
masif2002 <masif2002@outlook.com>
2023-05-10 07:17:01 +0530

    Add audio recording

--- src/components/ChatRoom.vue
+++ src/components/ChatRoom.vue
@@ -6,7 +6,7 @@
 
             <ul>
                 <li v-for="message of messages" :key="message.id">
-                    {{  message.text }}
+                    <ChatMessage :message="message" :owner="message.sender === user.uid"/>
                 </li>
             </ul>
 
@@ -14,9 +14,22 @@
             <button 
                 class="button is-success"
                 @click="addMessage(user.uid)"
+                :disabled = "!message || loading"
+                :class = "{ 'is-loading' : loading }"
             >
                 Send Message
             </button>
+
+            <h5>Record Audio</h5>
+
+            <button v-if="!recorder" @click="record()" class="button is-info">Record Voice</button>
+            <button v-else @click="stop()" class="button is-danger">Stop</button>
+
+            <br />
+
+            <audio v-if="newAudio" :src="newAudioURL" controls></audio>
+
+            <hr />
 
         </template>
     </UserComponent>
@@ -24,22 +37,29 @@
 
 <script>
 import UserComponent from './UserComponent.vue'
+import ChatMessage from './ChatMessage.vue'
 import { db } from '../firebase'
-import { collection, addDoc, query, orderBy } from 'firebase/firestore'
+import { collection, addDoc, query, orderBy, limitToLast } from 'firebase/firestore'
 
 export default {
-    components: {UserComponent},
+    components: { UserComponent, ChatMessage },
     data () {
         return {
             message: '',
             loading: false,
-            messages: [] // for realtime data fetching
+            messages: [], // for realtime data fetching
+            newAudio: null,
+            recorder: null,
         }
     },
     computed: {
         chatId() {
             return this.$route.params.id
         },
+        newAudioURL() {
+            // Creates a new URL for blob file 
+            return URL.createObjectURL(this.newAudio)
+        }
     },
     // Realtime Data fetching using VueFire
     watch: {
@@ -47,7 +67,7 @@ export default {
             immediate: true,
             handler(chatId) {
                 const collectionRef =  collection(db, 'chats', chatId, 'messages')
-                this.$firestoreBind('messages', query(collectionRef, orderBy('createdAt')))
+                this.$firestoreBind('messages', query(collectionRef, orderBy('createdAt'), limitToLast(10)))
             }
         }
     },
@@ -67,7 +87,57 @@ export default {
             })
 
             this.loading = false
+            this.message = ''
+        },
+
+        async record () {
+            this.newAudio = null
+
+            const stream = await navigator.mediaDevices.getUserMedia({
+                audio: true,
+                video: false
+            })
+
+            const options = { mimeType: "audio/webm" }
+            const recordedChunks = []
+            this.recorder = new MediaRecorder(stream, options)
+
+            this.recorder.addEventListener('dataavailable', e => {
+                
+                if (e.data.size > 0) {
+                    recordedChunks.push(e.data)
+                }
+            })
+
+            this.recorder.addEventListener('stop', () => {
+                this.newAudio = new Blob(recordedChunks);
+                console.log(this.newAudio);
+            })
+
+            this.recorder.start()
+        },
+
+        async stop() {
+            this.recorder.stop()
+            this.recorder = null
         }
     }
 }
 </script>
+
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  min-width: 500px;
+  background: #efefef;
+  border-radius: 0;
+}
+
+li {
+  display: flex;
+}
+</style>
